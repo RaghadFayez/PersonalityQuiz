c4494f7638d0ad72e2f326427079c576c840a5b0
R <raghad.fayez1@gmail.com>
2023-01-22 22:47:43 +0300

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Raghad Fayez on 29/06/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
